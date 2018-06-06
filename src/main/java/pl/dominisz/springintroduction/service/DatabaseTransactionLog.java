23cf8c4c35aa7bf23afcc048af0772cb298c4506
dominisz <dominisz@gmail.com>
2018-06-06 18:49:32 +0200

    Implement methods

--- src/main/java/pl/dominisz/springintroduction/service/DatabaseTransactionLog.java
+++ src/main/java/pl/dominisz/springintroduction/service/DatabaseTransactionLog.java
@@ -1,8 +1,19 @@
 package pl.dominisz.springintroduction.service;
 
+import pl.dominisz.springintroduction.exception.UnreachableException;
+import pl.dominisz.springintroduction.model.ChargeResult;
+
 /**
  * http://dominisz.pl
  * 06.06.2018
  */
 public class DatabaseTransactionLog implements TransactionLog {
+
+    public void logChargeResult(ChargeResult result) {
+
+    }
+
+    public void logConnectException(UnreachableException e) {
+
+    }
 }
--- src/main/java/pl/dominisz/springintroduction/service/PaypalCreditCardProcessor.java
+++ src/main/java/pl/dominisz/springintroduction/service/PaypalCreditCardProcessor.java
@@ -1,8 +1,16 @@
 package pl.dominisz.springintroduction.service;
 
+import pl.dominisz.springintroduction.model.ChargeResult;
+import pl.dominisz.springintroduction.model.CreditCard;
+
+import java.math.BigDecimal;
+
 /**
  * http://dominisz.pl
  * 06.06.2018
  */
 public class PaypalCreditCardProcessor implements CreditCardProcessor {
+    public ChargeResult charge(CreditCard creditCard, BigDecimal amount) {
+        return null;
+    }
 }
