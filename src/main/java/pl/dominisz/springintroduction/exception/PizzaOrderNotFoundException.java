479e5e5a632fc31a78d0405717c09bd392654470
dominisz <dominisz@gmail.com>
2018-06-08 20:19:56 +0200

    Response status for PizzaOrderNotFoundException

--- src/main/java/pl/dominisz/springintroduction/exception/PizzaOrderNotFoundException.java
+++ src/main/java/pl/dominisz/springintroduction/exception/PizzaOrderNotFoundException.java
@@ -1,8 +1,12 @@
 package pl.dominisz.springintroduction.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * http://dominisz.pl
  * 07.06.2018
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class PizzaOrderNotFoundException extends RuntimeException {
 }
